26dab4d3b59a5e24a10fe673b7851d450f5b20e6
Rober Quin <quinonesroberto92@gmail.com>
2024-11-21 14:14:27 -0600

    Modularización, creación de componentes y configuración de routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    PaisModule,
+    SharedModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'paisesApp';
+  title = 'paisessApp';
 }
