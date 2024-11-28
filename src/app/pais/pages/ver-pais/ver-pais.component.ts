3e75ac2e5ab2d7628875fcfa652cfa31548fd32e
Rober Quin <quinonesroberto92@gmail.com>
2024-11-28 13:06:32 -0600

    Finalización del compinente verPaisas

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,16 +10,27 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit{
 
+  pais!:Country;
+
   constructor(private activatedRoute:ActivatedRoute, private paisService:PaisService){
 
   }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id})=>{
-      this.paisService.getPaisPorAlpha(id).subscribe(pais=>{
-        console.log(pais);
-      })
-    })
-  }
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=>this.paisService.getPaisPorAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe( ([pais]) => this.pais = pais);
+    // this.activatedRoute.params.subscribe(({id})=>{
+    //   this.paisService.getPaisPorAlpha(id).subscribe(pais=>{
+    //     console.log(pais);
+    //   })
+    // })
+  }  
 
+  agregarPais(nuevo:Country){
+    this.pais=nuevo;
+  }
 }
