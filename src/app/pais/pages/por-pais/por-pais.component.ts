16d6b56eb05cdf056233eb88f1a43afcad46c6df
Rober Quin <quinonesroberto92@gmail.com>
2024-11-29 12:43:37 -0600

    Funcionalidad del componente porRegion y cierre de la aplicación

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,18 +4,25 @@ import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles:[`
+    li:{
+      cursor:pointer;
+    }`]
 })
 export class PorPaisComponent {
   termino:string='';
   hayError:boolean=false;
   paises:Country[]=[];
+  paisesSugeridos:Country[]=[];
+  mostrarSugerencia:boolean=false;
 
   constructor(private paisService:PaisService){}
 
   buscar(termino:string){
     this.hayError=false;
     this.termino=termino;
+    this.mostrarSugerencia=false;
     this.paisService.buscarPais(this.termino)
     .subscribe(paises=>{
       console.log(paises);
@@ -26,7 +33,18 @@ export class PorPaisComponent {
     });
   }
 
-  sugerencias(event:string){
+  sugerencias(termino:string){
     this.hayError=false;
+    this.termino=termino;
+    this.mostrarSugerencia=true;
+    //sugerencias
+    this.paisService.buscarPais(termino)
+    .subscribe(paises=>{
+      this.paisesSugeridos=paises.splice(0,5);
+    }, (err)=> this.paisesSugeridos=[])
+  }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
   }
 }
